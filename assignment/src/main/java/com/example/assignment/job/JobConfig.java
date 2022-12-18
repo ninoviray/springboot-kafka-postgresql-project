a0ddb0bc0d7737644d189de62a5e02fbf14ce499
Nino Viray <113711321+ninoviray@users.noreply.github.com>
2022-12-18 06:38:02 -0600

    Add files via upload

--- assignment/src/main/java/com/example/assignment/job/JobConfig.java
+++ assignment/src/main/java/com/example/assignment/job/JobConfig.java
@@ -4,24 +4,57 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class JobConfig {
 
-    /*
-    //Testing purposes, initialized table with data
+    //having issues with post command, CORS is blocking angular from reaching 8080
     @Bean
-    CommandLineRunner commandLineRunner(JobRepository repository){
-        return args -> {
-            Job calculatepi = new Job(1L, "Calculate Pi", "new");
-            Job handlethiserror = new Job("Handle This Error", "new");
-            Job writeaprogram = new Job("Write A Program", "new");
-
-            repository.saveAll(List.of(calculatepi, handlethiserror, writeaprogram));
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
         };
     }
-    */
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+//    //Testing purposes, initialized table with data
+//    @Bean
+//    CommandLineRunner commandLineRunner(JobRepository repository){
+//        return args -> {
+//            Job calculatepi = new Job(1L, "Calculate Pi", "new");
+//            Job handlethiserror = new Job("Handle This Error", "new");
+//            Job writeaprogram = new Job("Write A Program", "new");
+//
+//            repository.saveAll(List.of(calculatepi, handlethiserror, writeaprogram));
+//        };
+//    }
 
 }
